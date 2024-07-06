ab1f72a9445d5f7f9114ce843addff6f237522d4
garbich <131876643+garbich@users.noreply.github.com>
2024-07-06 15:55:26 +0300

    Add files via upload

---
+++ Task6-7/Task6-7/Calculation.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task6
+{
+    class Calculation
+    {
+        private int num = 0;
+        private int maxNum = 0;
+        private int minNum = Int32.MaxValue;
+        private double averageNum = 0;
+        private List<int> randomNum = new List<int>();
+
+        public int minNumber()
+        {
+            Thread thread1 = new Thread(() =>
+            {
+
+                foreach (var item in randomNum)
+                {
+                    if (minNum > item)
+                    {
+                        minNum = item;
+                    }
+                }
+
+            });
+            thread1.Start();
+
+
+            for (int i = 0; i <= randomNum.Count; i++)
+            {
+                thread1.Join();
+            }
+            return minNum;
+        }
+
+        public double averageNumber()
+        {
+            Thread thread2 = new Thread(() =>
+            {
+
+                foreach (var item in randomNum)
+                {
+                    averageNum += item;
+                }
+
+                averageNum /= randomNum.Count;
+
+
+
+
+            });
+            thread2.Start();
+
+
+            for (int i = 0; i <= randomNum.Count; i++)
+            {
+                thread2.Join();
+            }
+            return averageNum;
+        }
+
+        public int maxNumber()
+        {
+            Thread thread = new Thread(() =>
+            {
+
+                foreach (var item in randomNum)
+                {
+                    if (maxNum < item)
+                    {
+                        maxNum = item;
+                    }
+                }
+
+
+
+            });
+            thread.Start();
+
+
+            for (int i = 0; i <= randomNum.Count; i++)
+            {
+                thread.Join();
+            }
+            return maxNum;
+        }
+
+        public List<int> generateNums(int count)
+        {
+
+
+            Random random = new Random();
+
+
+            for (int i = 0; i < count; i++)
+            {
+                num = random.Next(1, 10000);
+                randomNum.Add(num);
+            }
+
+            return randomNum;
+
+        }
+    }
+}
---
+++ Task6-7/Task6-7/Form2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Task6
+{
+    public partial class Form2 : Form
+    {
+        private Calculation calculation = new Calculation();
+       
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = Int32.Parse(textBox1.Text);
+
+           
+            Task7(count);
+        }
+
+        public void Task7(int count)
+        {
+            List<int> list =  calculation.generateNums(count);
+            foreach (var item in list)
+            {
+
+                listBox1.Items.Add(item);
+            }
+
+            int maxNum = calculation.maxNumber();
+            int minNum = calculation.minNumber();
+            double averageNum = calculation.averageNumber();
+
+
+            listBox2.Items.Add("Max number: " + maxNum);
+            listBox2.Items.Add("Min number: " + minNum); 
+            listBox2.Items.Add("Average number: " + averageNum);
+
+        }
+        
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
